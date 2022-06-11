b5f078138aae3271b50c958718e2db70e6ae141f
Odric Roux-Paris <odricrouxparis@gmail.com>
2022-06-11 11:17:46 +0200

    Split architecture

---
+++ common/default.nix
@@ -0,0 +1,62 @@
+{ pkgs, ... }:
+
+{
+  imports = [ ./hardware-configuration.nix ];
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  
+
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/Paris";
+
+  i18n.defaultLocale = "en_US.utf8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "fr_FR.utf8";
+    LC_IDENTIFICATION = "fr_FR.utf8";
+    LC_MEASUREMENT = "fr_FR.utf8";
+    LC_MONETARY = "fr_FR.utf8";
+    LC_NAME = "fr_FR.utf8";
+    LC_NUMERIC = "fr_FR.utf8";
+    LC_PAPER = "fr_FR.utf8";
+    LC_TELEPHONE = "fr_FR.utf8";
+    LC_TIME = "fr_FR.utf8";
+  };
+
+  services.xserver.enable = true;
+
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  services.printing.enable = true;
+
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  nix = {
+    package = pkgs.nixFlakes; # or versioned attributes like nixVersions.nix_2_8
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+
+  environment.systemPackages = with pkgs; [ vim firefox vscode git ];
+
+  nixpkgs.config.allowUnfree = true;
+}
--- flake.nix
+++ flake.nix
@@ -25,58 +25,14 @@
   };
 
   outputs = { self, nixpkgs, home-manager, flake-utils, ... }@inputs: {
+
     formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt;
+
     nixosConfigurations.dev-machine = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ({ pkgs, ... }: {
-          imports = [ ./hardware-configuration.nix ];
-
-          boot.loader.systemd-boot.enable = true;
-          boot.loader.efi.canTouchEfiVariables = true;
-          boot.loader.efi.efiSysMountPoint = "/boot/efi";
-
-          networking.hostName = "nixos"; # Define your hostname.
-          # networking.wireless.enable = true;  
-
-          networking.networkmanager.enable = true;
-
-          time.timeZone = "Europe/Paris";
-
-          i18n.defaultLocale = "en_US.utf8";
-
-          i18n.extraLocaleSettings = {
-            LC_ADDRESS = "fr_FR.utf8";
-            LC_IDENTIFICATION = "fr_FR.utf8";
-            LC_MEASUREMENT = "fr_FR.utf8";
-            LC_MONETARY = "fr_FR.utf8";
-            LC_NAME = "fr_FR.utf8";
-            LC_NUMERIC = "fr_FR.utf8";
-            LC_PAPER = "fr_FR.utf8";
-            LC_TELEPHONE = "fr_FR.utf8";
-            LC_TIME = "fr_FR.utf8";
-          };
-          services.xserver.enable = true;
-
-          services.xserver.displayManager.gdm.enable = true;
-          services.xserver.desktopManager.gnome.enable = true;
-
-          services.xserver = {
-            layout = "us";
-            xkbVariant = "";
-          };
-
-          services.printing.enable = true;
-
-          sound.enable = true;
-          hardware.pulseaudio.enable = false;
-          security.rtkit.enable = true;
-          services.pipewire = {
-            enable = true;
-            alsa.enable = true;
-            alsa.support32Bit = true;
-            pulse.enable = true;
-          };
+          imports = [ ./common ];
 
           users.users.odric = {
             isNormalUser = true;
@@ -84,27 +40,8 @@
             extraGroups = [ "networkmanager" "wheel" ];
           };
 
-          nixpkgs.config.allowUnfree = true;
-
-          environment.systemPackages = with pkgs; [
-            vim
-            wget
-            firefox
-            discord
-            vscode
-            git
-            gcc
-          ];
-
           system.stateVersion = "22.05"; # Did you read the comment?
 
-          nix = {
-            package =
-              pkgs.nixFlakes; # or versioned attributes like nixVersions.nix_2_8
-            extraOptions = ''
-              experimental-features = nix-command flakes
-            '';
-          };
         })
       ];
     };
