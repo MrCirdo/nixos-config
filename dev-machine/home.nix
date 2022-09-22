eacd814541d2bf81f72813be0ff9fb2a04df2258
Odric Roux-Paris <odricrouxparis@gmail.com>
2022-09-22 10:23:29 +0200

    nix fmt

--- dev-machine/home.nix
+++ dev-machine/home.nix
@@ -7,46 +7,46 @@
     username = "odric";
     homeDirectory = "/home/odric";
     stateVersion = "22.05";
-    packages =
-      let
-        python-packages = (pkgs.python310.withPackages (ps: with ps; [ pwntools z3 angr numpy matplotlib jinja2])).override (args: { ignoreCollisions = true; });
-      in
-      with pkgs;
-      [
-        feh
-        tree
-        gdb
-        gcc
-        glibc
-        gnumake
-        automake
-        autogen
-        autoconf
-        cmake
-        zip
-        unzip
-        bitwarden
-        thunderbird
-        mpv
-        jellyfin-mpv-shim
-        bear
-        htop
-        ctags
-        ccls
-        clang-tools
-        ghidra-bin
-        man-pages
-        man-pages-posix
-        tmux
-        vagrant
-        nixpkgs-fmt
-        wayland
-        python-packages
-        chromium
-        openssl
-        spotify
-        signal-desktop
-      ];
+    packages = let
+      python-packages = (pkgs.python310.withPackages
+        (ps: with ps; [ pwntools z3 angr numpy matplotlib jinja2 ])).override
+        (args: { ignoreCollisions = true; });
+    in with pkgs; [
+      feh
+      tree
+      gdb
+      gcc
+      glibc
+      gnumake
+      automake
+      autogen
+      autoconf
+      cmake
+      zip
+      unzip
+      bitwarden
+      thunderbird
+      mpv
+      jellyfin-mpv-shim
+      bear
+      htop
+      ctags
+      ccls
+      clang-tools
+      ghidra-bin
+      man-pages
+      man-pages-posix
+      tmux
+      vagrant
+      nixpkgs-fmt
+      wayland
+      python-packages
+      chromium
+      openssl
+      spotify
+      signal-desktop
+    ];
+  };
 
   programs = import ./programs { inherit lib pkgs; };
   services = import ./services { inherit pkgs; };
