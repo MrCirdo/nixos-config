c94b64e03c090f36b30da1dcf8cdbc83d5ad720c
Odric Roux-Paris <odricrouxparis@gmail.com>
2022-09-22 10:10:20 +0200

    feat(gpg): add gpg.

--- dev-machine/programs/git.nix
+++ dev-machine/programs/git.nix
@@ -4,7 +4,5 @@
   enable = true;
   userEmail = "odricrouxparis@gmail.com";
   userName = "Odric Roux-Paris";
-  signing = {
-    key = "0x8EA8FD02F063AB8F";
-  };
+  signing = { key = "0x8EA8FD02F063AB8F"; };
 }
