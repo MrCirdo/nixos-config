78950f16990793cffb361152db4ce55e90ecdcbd
Odric Roux-Paris <odricrouxparis@gmail.com>
2022-09-22 10:09:32 +0200

    fix: update to latest version

--- dev-machine/desktop.nix
+++ dev-machine/desktop.nix
@@ -14,10 +14,7 @@
   xdg.portal = {
     enable = true;
     wlr.enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-wlr
-    ];
-    gtkUsePortal = true;
+    extraPortals = with pkgs; [ xdg-desktop-portal-wlr ];
   };
 
   services.greetd = {
--- dev-machine/services/picom.nix
+++ dev-machine/services/picom.nix
@@ -3,10 +3,6 @@
 {
   enable = false;
   experimentalBackends = true;
-  blur = true;
-  extraOptions = ''
-    corner-radius = 5.0;
-  '';
   #extraOptions = ''
   #  corner-radius = 5.0;
   #  blur-method = "kawase";
