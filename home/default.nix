40a50c3a229308621f71a2db7123565ff7618ae1
Odric Roux-Paris <odricrouxparis@gmail.com>
2022-11-02 15:04:31 +0100

    feat(font): remove font

--- home/default.nix
+++ home/default.nix
@@ -13,20 +13,10 @@
   };
 
   fonts.fonts = with pkgs; [
-    noto-fonts
-    noto-fonts-emoji
-    dejavu_fonts
     nerdfonts
-    roboto
-    roboto-mono
-    victor-mono
     ubuntu_font_family
-    carlito
-    vegur
     source-code-pro
-    jetbrains-mono
     font-awesome
-    corefonts
     (nerdfonts.override { fonts = [ "FiraCode" ]; })
   ];
 
