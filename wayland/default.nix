bb180fa11fd06dfe4cc160ae28b5c90e28b9810f
Odric Roux-Paris <odricrouxparis@gmail.com>
2022-11-04 09:46:43 +0100

    fix(login): add sway auto login

--- wayland/default.nix
+++ wayland/default.nix
@@ -1,20 +1,39 @@
 { pkgs, ... }:
+let
+launchSway = pkgs.writeShellScriptBin "launch_sway.sh"
+  ''
+export XDG_SESSION_TYPE=wayland
+export XDG_SESSION_DESKTOP=sway
+export XDG_CURRENT_DESKTOP=sway
+export MOZ_ENABLE_WAYLAND=1
+export QT_QPA_PLATFORM=wayland
+export SDL_VIDEODRIVER=wayland
+export _JAVA_AWT_WM_NONREPARENTING=1
 
-{
-  programs = {
-    sway = { enable = true; };
-
-    light.enable = true;
-  };
-
-  services.xserver.displayManager.gdm = {
-    enable = true;
-    wayland = true;
-  };
-
-  security.polkit.enable = true;
-  environment.loginShellInit = ''
-  [[ "$(tty)" == /dev/tty1 ]] && sway
+${pkgs.sway}/bin/sway
   '';
+in
+  {
+    programs = {
+      sway = { enable = true; };
+      light.enable = true;
+    };
 
-}
+    services.xserver.displayManager.gdm = {
+      enable = true;
+      wayland = true;
+    };
+
+    security.polkit.enable = true;
+    services.greetd = {
+      enable = true;
+      settings = rec {
+        initial_session = {
+          command = "${launchSway}/bin/launch_sway.sh";
+          user = "odric";
+        };
+        default_session = initial_session;
+      };
+    };
+
+  }
