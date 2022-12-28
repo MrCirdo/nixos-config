8bc03f48f885931e36338e3d780647b0741a303d
Odric Roux-Paris <odricrouxparis@gmail.com>
2022-12-28 19:18:28 +0100

    remove nixpkgs-wayland

--- flake.nix
+++ flake.nix
@@ -5,16 +5,12 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    nixpkgs-wayland = {url = "github:nix-community/nixpkgs-wayland";};
-    nixpkgs-wayland.inputs.master.follows = "master";
   };
 
   outputs = {
     self,
     nixpkgs,
     home-manager,
-    nixpkgs-wayland,
     ...
   }: {
     formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
@@ -29,7 +25,7 @@
           home-manager.nixosModules.home-manager
           ./home
           ./wayland
-          {nixpkgs.overlays = [nixpkgs-wayland.overlay] ++ overlays;}
+          {nixpkgs.overlays = overlays;}
         ];
       };
     };
