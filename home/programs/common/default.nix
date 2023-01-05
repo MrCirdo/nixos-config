6ae3920ddd3cea19d10a388f08d94bd0a032fb1e
Odric Roux-Paris <odricrouxparis@gmail.com>
2023-01-05 22:07:55 +0100

    feat(helix): change cursor behavior settings.

--- home/programs/common/default.nix
+++ home/programs/common/default.nix
@@ -108,8 +108,13 @@
     helix = {
       enable = true;
       settings = {
-          theme = "onedark";
+        theme = "onedark";
+        editor.cursor-shape = {
+          insert = "bar";
+          normal = "block";
+          select = "underline";
         };
+      };
     };
   };
 
