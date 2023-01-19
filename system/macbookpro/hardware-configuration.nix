08bc4eb4e2e6a9bc66f09535c72ce8ab19bd5088
Odric Roux-Paris <odricrouxparis@gmail.com>
2023-01-19 08:57:36 +0100

    tmpfs: Disable tmp on tmpfs
    Sometime, some operation have to use
    a lot of space in /tmp, and it's failed
    because my ram has not enough space.

--- system/macbookpro/hardware-configuration.nix
+++ system/macbookpro/hardware-configuration.nix
@@ -34,7 +34,7 @@
     {device = "/dev/disk/by-uuid/6e5f19db-2ca8-40d7-834d-93073b6e788f";}
   ];
 
-  boot.tmpOnTmpfs = true;
+  boot.tmpOnTmpfs = false;
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
