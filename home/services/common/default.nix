7b058ae67bc1fefa6ae2b6655ffd41a19f0655ed
Odric Roux-Paris <odricrouxparis@gmail.com>
2023-01-22 00:29:45 +0100

    gpg: change pinentry.

--- home/services/common/default.nix
+++ home/services/common/default.nix
@@ -1,11 +1,14 @@
-{...}: {
+{config, ...}: {
   services = {
     gpg-agent = {
       enable = true;
       enableZshIntegration = true;
       enableBashIntegration = true;
       enableSshSupport = true;
-      pinentryFlavor = "qt";
+      pinentryFlavor =
+        if config.gnome.enable
+        then "gnome3"
+        else "qt";
       extraConfig = "auto-expand-secmem 0x3000000\n";
     };
   };
