cfc3f5dd4b7e3d3d3f71eeb8af91cbe927dd5c3e
Odric Roux-Paris <odricrouxparis@gmail.com>
2023-04-10 10:58:43 +0200

    Enable fingerprint auth

--- machine/xps9500/default.nix
+++ machine/xps9500/default.nix
@@ -2,5 +2,19 @@
   gnome.enable = true;
   sway.enable = false;
   hyprland.enable = false;
+
+  services.fprintd = {
+    enable = true;
+    tod = {
+      enable = true;
+      driver = pkgs.libfprint-2-tod1-goodix;
+    };
+  };
+
+  security.pam.services = {
+    login.fprintAuth = true;
+    sudo.fprintAuth = true;
+  };
+
   imports = [./system];
 }
