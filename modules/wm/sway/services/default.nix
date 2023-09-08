7cdf55b923034dfab5f9ae9b4a14b849475f1e76
Odric Roux-Paris <odricrouxparis@gmail.com>
2023-09-08 12:09:16 +0200

    sway: Add automatic suspend

--- modules/wm/sway/services/default.nix
+++ modules/wm/sway/services/default.nix
@@ -14,6 +14,7 @@
             duskTime = "22:00";
             temperature.night = 2500;
           };
+
           mako = {
             enable = true;
             backgroundColor = config.theme.focusColor;
@@ -21,6 +22,7 @@
             defaultTimeout = 10000;
             borderRadius = 8;
           };
+
           swayidle = {
             enable = true;
             events = [
@@ -33,7 +35,31 @@
               {
                 timeout = 300;
                 command = "${pkgs.swaylock-effects}/bin/swaylock -f -C $HOME/.config/swaylock-effects/config";
+                # command = "${pkgs.swaylock-effects}/bin/swaylock -f -C $HOME/.config/swaylock-effects/config; ${pkgs.sway}/bin/swaymsg output '*' dpms off";
+                # resumeCommand = "${pkgs.sway}/bin/swaymsg output '*' dpms on";
               }
+              (let
+                suspendOrTurnOffScreen = pkgs.writeShellScriptBin "suspendOrTurnOffScreen.sh" ''
+                  status_charging=$(cat /sys/class/power_supply/*/status)
+
+                  if [ $status_charging = "Discharging" ];
+                  then
+                    echo "Discarging"
+                    ${pkgs.systemd}/bin/systemctl suspend
+                  else
+                    echo "Charging"
+                    # I comment this for later.
+                    # wlroots bug, I disable this feature.
+                    # I can't re-enable my screens after :)
+                    # lol
+                    # ${pkgs.sway}/bin/swaymsg output '*' dpms off
+                  fi
+                '';
+              in {
+                timeout = 360;
+                command = "${suspendOrTurnOffScreen}/bin/suspendOrTurnOffScreen.sh";
+                # resumeCommand = "${pkgs.sway}/bin/swaymsg output '*' dpms on";
+              })
             ];
           };
         };
