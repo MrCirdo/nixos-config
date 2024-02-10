4b091c13059cc60059c87d37e5a1c22d61c702bf
Odric Roux-Paris <odricrouxparis@gmail.com>
2024-02-10 18:58:31 +0100

    sway: change theme

--- modules/wm/sway/themes.nix
+++ modules/wm/sway/themes.nix
@@ -6,17 +6,17 @@
   tailwindcss = config.theme.tailwindcss;
 in {
   theme = lib.mkIf config.sway.enable {
-    backgroundColor = tailwindcss.sky."100";
-    backgroundColor2 = tailwindcss.sky."200";
-    backgroundColor3 = tailwindcss.sky."300";
+    backgroundColor = tailwindcss.indigo."950";
+    backgroundColor2 = tailwindcss.indigo."900";
+    backgroundColor3 = tailwindcss.indigo."800";
 
-    focusColor = tailwindcss.sky."500";
-    focusColor2 = tailwindcss.sky."400";
-    focusColor3 = tailwindcss.sky."300";
+    focusColor = tailwindcss.indigo."500";
+    focusColor2 = tailwindcss.indigo."400";
+    focusColor3 = tailwindcss.indigo."300";
 
-    textColor = tailwindcss.sky."900";
-    textColor2 = tailwindcss.sky."800";
-    textColor3 = tailwindcss.sky."700";
+    textColor = tailwindcss.indigo."100";
+    textColor2 = tailwindcss.indigo."200";
+    textColor3 = tailwindcss.indigo."300";
 
     alertColor = tailwindcss.red."500";
   };
