7ee0ebaef4543ebd377bc0be6a9bbcac1c8a7fca
Odric Roux-Paris <odricrouxparis@gmail.com>
2024-02-29 15:35:02 +0100

    theme: add theme for the other computer

--- machine/p16sgen2/default.nix
+++ machine/p16sgen2/default.nix
@@ -1,6 +1,7 @@
 {
   pkgs,
   lib,
+  config,
   ...
 }: {
   gnome.enable = false;
@@ -54,6 +55,24 @@
   security.pam.services = {
     login.fprintAuth = true;
     sudo.fprintAuth = true;
+  };
+
+  theme = let
+    tailwindcss = config.theme.tailwindcss;
+  in {
+    backgroundColor = tailwindcss.indigo."950";
+    backgroundColor2 = tailwindcss.indigo."900";
+    backgroundColor3 = tailwindcss.indigo."800";
+
+    focusColor = tailwindcss.indigo."500";
+    focusColor2 = tailwindcss.indigo."400";
+    focusColor3 = tailwindcss.indigo."300";
+
+    textColor = tailwindcss.indigo."100";
+    textColor2 = tailwindcss.indigo."200";
+    textColor3 = tailwindcss.indigo."300";
+
+    alertColor = tailwindcss.red."500";
   };
 
   imports = [./system];
