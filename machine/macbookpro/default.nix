f753c23131192558f757e982e57b6e5af281ae96
Odric Roux-Paris <odricrouxparis@gmail.com>
2024-02-29 15:35:02 +0100

    theme: add theme for the other computer

--- machine/macbookpro/default.nix
+++ machine/macbookpro/default.nix
@@ -1,6 +1,24 @@
-{...}: {
+{config, ...}: {
   gnome.enable = false;
   sway.enable = true;
+
+  theme = let
+    tailwindcss = config.theme.tailwindcss;
+  in {
+    backgroundColor = tailwindcss.indigo."950";
+    backgroundColor2 = tailwindcss.indigo."900";
+    backgroundColor3 = tailwindcss.indigo."800";
+
+    focusColor = tailwindcss.indigo."500";
+    focusColor2 = tailwindcss.indigo."400";
+    focusColor3 = tailwindcss.indigo."300";
+
+    textColor = tailwindcss.indigo."100";
+    textColor2 = tailwindcss.indigo."200";
+    textColor3 = tailwindcss.indigo."300";
+
+    alertColor = tailwindcss.red."500";
+  };
 
   imports = [./system];
 }
