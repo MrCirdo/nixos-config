0458501c71dc1cb70c04ece4bacf05d6e736f430
Odric Roux-Paris <odricrouxparis@gmail.com>
2024-04-29 21:48:12 +0200

    sound drop compatibility with other audio server

--- common/system/services/default.nix
+++ common/system/services/default.nix
@@ -3,10 +3,7 @@
   services = {
     pipewire = {
       enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
       pulse.enable = true;
-      wireplumber.enable = true;
     };
     tailscale.enable = true;
     openssh = {
