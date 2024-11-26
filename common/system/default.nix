8f5ce45ae45c15ce1d9d879f8779e80f7f708eae
Odric Roux-Paris <odric@roux-paris.fr>
2024-11-26 11:56:45 +0100

    change dns server
    From Cloudfare to Quad9

--- common/system/default.nix
+++ common/system/default.nix
@@ -104,7 +104,12 @@
   };
 
   system.stateVersion = "23.05"; # Did you read the comment?
-  networking.nameservers = ["1.1.1.1" "1.0.0.1"];
+  networking.nameservers = [
+    "9.9.9.9"
+    "149.112.112.112"
+    "2620:fe::fe"
+    "2620:fe::9"
+  ];
 
   powerManagement.enable = true;
 
