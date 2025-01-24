6a84a9cb4f70c2ce8841d709964609bdcf9469dc
Odric Roux-Paris <odric@roux-paris.fr>
2025-01-24 20:02:33 +0100

    sway: enhance tlp settings

--- modules/wm/sway/services/default.nix
+++ modules/wm/sway/services/default.nix
@@ -4,7 +4,15 @@
   ...
 }: {
   services = lib.mkIf config.sway.enable {
-    tlp.enable = true;
+    tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+      };
+    };
   };
   home-manager.sharedModules = [
     (
