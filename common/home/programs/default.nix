8deb3868c175ea1f106fd5aeddf2c45ac9c02445
Odric Roux-Paris <odric@roux-paris.fr>
2025-02-12 13:57:19 +0100

    update gpg key

--- common/home/programs/default.nix
+++ common/home/programs/default.nix
@@ -299,7 +299,7 @@
         {
           source = pkgs.fetchurl {
             url = "https://github.com/MrCirdo.gpg";
-            hash = "sha256-VcMM8wf+1zPExshINV/RLRUjN/LhulncP7OfJmtgKzM=";
+            hash = "sha256-N5CwHYyDGyvPe2uum4bvt3pqtHsmOM7ken5YstK0hSo=";
           };
           trust = "ultimate";
         }
