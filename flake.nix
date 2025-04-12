6b80aa5d663ff1930dc01217d7eff77e6c3bae09
Odric Roux-Paris <odric@roux-paris.fr>
2025-04-12 16:31:53 +0200

    groot: use right module from nixos-hardware

--- flake.nix
+++ flake.nix
@@ -98,9 +98,7 @@
           ++ [
             ./machine/p16sgen2
             lanzaboote.nixosModules.lanzaboote
-            nixos-hardware.nixosModules.common-cpu-amd
-            nixos-hardware.nixosModules.common-pc-laptop
-            nixos-hardware.nixosModules.common-pc-ssd
+            nixos-hardware.nixosModules.lenovo-thinkpad-p16s-amd-gen2
           ];
       };
     };
