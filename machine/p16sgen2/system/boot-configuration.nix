518c7a60a7eb5256c96fb3b100b0fe856a9c1d81
Odric Roux-Paris <odric@roux-paris.fr>
2025-06-15 11:06:27 +0200

    groot: add tpm2 options to unlock root partition

--- machine/p16sgen2/system/boot-configuration.nix
+++ machine/p16sgen2/system/boot-configuration.nix
@@ -16,6 +16,7 @@
       systemd.enable = true;
       luks.devices."root" = {
         device = "/dev/disk/by-uuid/07425910-1a81-4a04-8366-2ce9560fb012";
+        crypttabExtraOpts = [ "tpm2-device=auto" "tpm2-measure-pcr=yes" ];
       };
     };
     lanzaboote = {
