a1921c4911ec96457eb5f103bb8a5057986b2ffb
Odric Roux-Paris <odric@roux-paris.fr>
2025-11-04 21:10:50 +0100

    shell: improve lldb

--- shells/llvm.nix
+++ shells/llvm.nix
@@ -1,14 +1,21 @@
-{
-  pkgs,
-  ...
-}:
-  with pkgs;
-    mkShell {
-      inputsFrom = [
-        llvmPackages.llvm
-      ];
-      buildInputs = [
-        llvmPackages.clang
-        llvmPackages.compiler-rt
-      ];
-    }
+{pkgs, ...}:
+with pkgs;
+  mkShell {
+    packages = [
+      libllvm
+      sccache
+    ];
+    inputsFrom = [
+      llvmPackages.llvm
+    ];
+    buildInputs = [
+      llvmPackages.clang
+      llvmPackages.compiler-rt
+    ];
+    shellHook = ''
+      export CXX=clang++
+      export CC=clang
+      export SCCACHE_SERVER_UDS=/var/run/sccache/sccache.sock
+      export SCCACHE_CACHE_SIZE=500000000000
+    '';
+  }
